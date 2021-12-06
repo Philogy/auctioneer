fceeb6b3bd37f9cd801106a4625abf62906b1f82
barbosa7 <81977215+barbosa7@users.noreply.github.com>
2021-12-07 00:55:09 +0100

    Update Factory.sol

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,26 +2,40 @@
 pragma solidity ^0.8.10;
 
 import "./AuctionImplementation.sol";
+import "@optionality.io/clone-factory/contracts/CloneFactory.sol";
 
 /// @author Tomás Barbosa
-contract Factory {
-    
+contract Factory is AuctionImplementation{
+    // I don't think we need a function to get the Auctions, since this would mean storing them and havinh to always change state variables
+    // I suggest we use the the getPastEvents web3.js function on the dapp instead and filter through the auctions that are active 
+    // this way we don't have to change state variables everytime we create a new auction decreasing the amount of gas necessary
+    address internal auctionImplementation;
 
-    function getAuction(AuctionImplementation _auction) internal view {
-
+    constructor(address _auctionImplementation){
+        auctionImplementation = _auctionImplementation;
     }
 
+    /// @dev setter for the implementation in case we want to upgrade the implementation later. this will only affect the auctions created after the new
+    /// implementation was set
+
+    function setAuctionImplementation(address _auctionImplementation) external onlyOwner {
+        auctionImplementation = _auctionImplementation;
+    }
+    
+    ///@dev creating a proxy of the AuctionImplementation and emitting an event informing that we did so
+    
     function createAuctions(address _beneficiary,
         uint256 _startingBid,
         uint256 _minIncrease,
         uint256 _auctionEnd,
         uint256 _minimumEnd) 
-        external returns(boolean){
-            AuctionImplementation aI = AuctionImplementation.init(_beneficiary, _startingBid, _minIncrease, _auctionEnd, _minimumEnd);
-            if (aI != 0) {
-                emit Init(_beneficiary, _startingBid, _auctionEnd);
-                return true;
-            }
-            return(false);
+        external {
+            address clone = createClone(auctionImplementation);
+            auctionImplementation(clone).init(_beneficiary,
+                _startingBid,
+                _minIncrease,
+                _auctionEnd,
+                _minimumEnd);
+            emit Init(_beneficiary, _startingBid, _auctionEnd);
     }
 }
